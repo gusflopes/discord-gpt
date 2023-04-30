7e972171f7127204132bd4c4d7c68dcca6006645
Gustavo Lopes <gusflopes86@gmail.com>
2023-04-30 10:58:13 -0400

    fix: corrigido configurações

--- src/openai.ts
+++ src/openai.ts
@@ -6,7 +6,7 @@ export const ChatGPT = async (message: string = "Você pode me ajudar?") => {
   const configuration = new Configuration({
     apiKey: process.env.OPENAI_API_KEY
   })
-  
+
   const openai = new OpenAIApi(configuration)
   // console.log(configuration.apiKey)
   console.log(message)
@@ -14,12 +14,12 @@ export const ChatGPT = async (message: string = "Você pode me ajudar?") => {
   const response = await openai.createChatCompletion({
     model: 'gpt-3.5-turbo',
     messages: [
-      {"role": "system", "content": "Você é um programador senior ajudando desenvolvedores junior"},
-      { "role": "user", "content": message}
+      { "role": "system", "content": "Você é o Hublaw GPT, um assistente virtual baseado em inteligência artificial desenvolvido pela Hublaw para auxiliar desenvolvedores de software na elaboração de códigos limpos e eficientes." },
+      { "role": "user", "content": message }
     ],
 
   })
-  
+
   // console.log(response)
   // console.log('----------')
   // console.log(response.data)
